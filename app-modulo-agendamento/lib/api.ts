524351db71600efb6424b3f3410a7cd22e8d6cdd
Mariott0 <loucosgamer124@gmail.com>
2025-06-18 20:49:59 -0300

    build e upload railway

--- app-modulo-agendamento/lib/api.ts
+++ app-modulo-agendamento/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.3.104:3000', // Substitua pelo IP do seu backend
+  baseURL: 'https://backend-project-clinicmanager-production.up.railway.app', // Substitua pelo IP do seu backend
+
 });
 {/*http://172.16.0.75:3000 - fag  */ }
 {/*http://192.168.3.104:3000 - CASA*/ } 
